22708c070710e5145213dfda59d5fbe3bac072e4
michacassola <mich@casso.la>
2024-03-03 18:43:48 +0100

    Create unypkg-openresty-build.sh

---
+++ unypkg-openresty-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+######################################################################################################################
+### Download
+# shellcheck disable=SC1091
+source /uny/uny/build/download_functions
+mkdir -pv /uny/sources
+cd /uny/sources || exit
+
+pkgname="openresty"
+pkggit="https://github.com/openresty/openresty.git refs/tags/v*"
+gitdepth="--depth=1"
+
+### Get version info from git remote
+# shellcheck disable=SC2086
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]*$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "v[0-9.]*" | sed "s|v||")"
+
+check_for_repo_and_create
+git_clone_source_repo
+
+version_details
+archiving_source
